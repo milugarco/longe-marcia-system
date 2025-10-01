8695a6b0eeca7be485838da1ebb4607e95e94df3
Mike <nosapien2@gmail.com>
2025-10-01 09:47:42 -0300

    test

--- src/app/api/employees/[id]/route.ts
+++ src/app/api/employees/[id]/route.ts
@@ -1,4 +1,4 @@
-// @ts-ignore
+// @ts-expect-error
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,4 +1,4 @@
-// @ts-ignore
+// @ts-expect-error
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { hash } from "bcryptjs";
